99fa4fc771beed3beef3d06dfab2e2ef1773ed21
James Min <jmin499@gmail.com>
2019-10-11 20:40:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,32 @@
 # require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(emoticons)
+  emoticons = YAML.load_file('./lib/emoticons.yml')
+  
+  emotes1 = {}
+  emotes2 = {}
+
+  emoticons.each { |k, v|
+    japanese_emotes = v[1]
+    meaning = k
+    emotes1[japanese_emotes] = meaning
+    emotes2[v[0]] = v[1]
+  }
+  
+  new_hash = {
+    :get_meaning =>  emotes1,
+    :get_emoticon => emotes2 
+  }
+  
+  new_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(emoticons, emot)
+  data = load_library(emoticons)
+  
+  
+  
 end
 
 def get_english_meaning
